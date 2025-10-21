0483fc16ce08ab31381c9f133d139f15b8dd6e92
YungFritz <fuzpaladium@gmail.com>
2025-10-21 08:52:30 +0200

    Update types.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,1 +1,20 @@
+export type Chapter = {
+  id: string;
+  name: string;
+  number: number;
+  lang: string;
+  releaseDate: string;
+  pages: string[]; // data URLs
+};
 
+export type Series = {
+  id: string;
+  title: string;
+  slug: string;
+  tags: string[];
+  cover?: string; // data URL
+  description?: string;
+  chapters: Chapter[];
+  views?: number;
+  hot?: boolean;
+};
