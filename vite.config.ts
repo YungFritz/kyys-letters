e4d4a41a8c54311ae217f52188d9e47f66ead30b
YungFritz <fuzpaladium@gmail.com>
2025-10-21 08:20:11 +0200

    Enhance Vite config with build options
    Added build configuration with multiple entry points.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        admin: "admin.html",
+        addManga: "add-manga.html",
+        addChapitre: "add-chapitre.html",
+        editeManga: "edit-manga.html", // ou edite-manga.html selon ton nom
+      },
+    },
+  },
+});
