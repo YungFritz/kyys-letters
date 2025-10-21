f3f30f346ef892213bfcdbce164c050d8c052c69
YungFritz <fuzpaladium@gmail.com>
2025-10-21 10:04:52 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public', // <-- on garde /public pour nos pages statiques
   build: {
+    outDir: 'dist',
+    emptyOutDir: true,
     rollupOptions: {
+      // On NE déclare que index.html comme entrée à builder
       input: {
-        main: "index.html",
-        admin: "admin.html",
-        addManga: "add-manga.html",
-        addChapitre: "add-chapitre.html",
-        editeManga: "edit-manga.html", // ou edite-manga.html selon ton nom
+        main: resolve(__dirname, 'index.html'),
       },
     },
   },
-});
+})
