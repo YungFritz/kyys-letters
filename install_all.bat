f8a772d575e442eb1e09d9a0dcc2e3629095e609
YungFritz <fuzpaladium@gmail.com>
2025-11-29 11:03:45 +0100

    Create install_all.bat to install Python packages

---
+++ install_all.bat
@@ -0,0 +1,4 @@
+@echo off
+pip install --upgrade pip
+pip install numpy pandas pillow opencv-python requests tqdm matplotlib scipy scikit-learn pyinstaller beautifulsoup4 lxml pydub moviepy python-dotenv fastapi uvicorn rich colorama aiohttp pytz schedule
+pause
